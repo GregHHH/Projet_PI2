3c14ce115549715b4901b50910b35762b9752ab9
Gregory Haton <greg@MacBook-Pro-de-Gregory.local>
2019-11-18 14:59:59 +0100

    encore plus propre

--- lil_ft.c
+++ lil_ft.c
@@ -19,6 +19,7 @@ int aff_content(FILE* fichier)
 		if (caractereActuel != EOF)
 			printf("%c", caractereActuel);
     } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+	printf("\n");
 	fclose(fichier);
 	return 0;
 }
