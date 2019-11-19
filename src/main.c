2669c0dc45921da69a649867bacb5fd8ed572792
Gregory Haton <greg@MacBook-Pro-de-Gregory.local>
2019-11-19 09:19:13 +0100

    ajout condition arguments

--- src/main.c
+++ src/main.c
@@ -1,34 +1,42 @@
 #include "ft.h"
 
-int main(void)
+int main(int ac, char **av)
 {
+	
+	char *filename = av[1];
 	FILE* fichier = NULL;
 	FILE* term = NULL;
-	fichier = fopen("src/test.logo", "r+");
-
-	if (fichier == NULL)
+	fichier = fopen(filename, "r+");
+	
+	if (ac != 2)
 	{
-        //* On affiche un message d'erreur si le fichier ne peut pas s'ouvrir ou si il se trouve au mauvais endroit
-        printf("Impossible d'ouvrir le fichier");
-		term = fopen("term.logo", "w+");
-		aff_content(term);
-
+		printf("Entrez le nom du fichier à interpréter");
+		return -1;
 	}
-
-	else if (fichier != NULL && ft_size(fichier) == 0)
+	else
 	{
-        printf("Fichier vide!\n");
-		term = fopen("term.logo", "w+");
+		if (fichier == NULL)
+		{
+        	//* On affiche un message d'erreur si le fichier ne peut pas s'ouvrir ou si il se trouve au mauvais endroit
+    	   	printf("Impossible d'ouvrir le fichier!\n Le fichier n'existe pas ou ne se trouve pas dans le bon dossier");
+			term = fopen("src/term.logo", "w+");
+			aff_content(term);
+		}
 
-		aff_content(term);
+		else if (fichier != NULL && ft_size(fichier) == 0)
+		{
+    	    printf("Fichier vide!\n");
+			term = fopen("src/term.logo", "w+");
 
+			aff_content(term);
+		}
+
+    	else if (fichier != NULL && ft_size(fichier) != 0)
+    	{ 
+			printf("Le fichier contient:\n\n");
+			aff_content(fichier);
+    	}
+		remove("src/term.logo");
 	}
-
-    else if (fichier != NULL && ft_size(fichier) != 0)
-    { 
-		printf("Le fichier contient:\n\n");
-		aff_content(fichier);
-    }
-	remove("term.logo");
     return 0;
 }
